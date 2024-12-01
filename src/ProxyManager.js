3ba7caac0c0df96a5d141a5fbc4e66d3d9546175
toelana <toelana@gmail.com>
2024-12-02 02:56:07 +0700

    Update ProxyManager.js

--- src/ProxyManager.js
+++ src/ProxyManager.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const fs = require('fs');
 
 const PROXY_SOURCES = {
-  'SERVER 1': 'https://files.ramanode.top/airdrop/grass/server_1.txt',
+  'SERVER 1': 'https://s3.filebin.net/filebin/6b50b27d442ac1ba4a077687b0a23f680881727be935e9f65eb0567de83f65eb/0715903c494cd313b486599d5a32fec22afda7f883b7bc9f2047a288b90b3c00?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=7pMj6hGeoKewqmMQILjm%2F20241201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241201T195524Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&response-cache-control=max-age%3D60&response-content-disposition=filename%3D%22proxy.txt%22&response-content-type=text%2Fplain%3B%20charset%3Dutf-8&X-Amz-Signature=9b629e673eea08e08a6c7f876748f88fe58dd97e965a6fd9ebd02108a7653d12',
   'SERVER 2': 'https://files.ramanode.top/airdrop/grass/server_2.txt',
   'SERVER 3': 'https://files.ramanode.top/airdrop/grass/server_3.txt',
   'SERVER 4': 'https://files.ramanode.top/airdrop/grass/server_4.txt',
